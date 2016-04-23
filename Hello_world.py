6460685adc64609d939a83b5c05ccbb8455862a5
h <h>
2016-04-24 02:25:19 +0300

    - working on baseπˆ

--- Hello_world.py
+++
@@ -1,8 +0,0 @@
-x = "dima"
-words = []
-while x != "end":
-    x = input("Enter a word")
-    if x != "end":
-        words.append(x)
-for word in words:
-    print(word + " " + str(len(word)))
--- digger.py
+++ digger.py
@@ -1,6 +1,12 @@
 import threading
 from tkinter import *  # Importing the Tkinter (tool box) library
-from winsound import *
+from terrain import *
+
+PLAYER_COLOR = "#689F38"
+
+itIsWin = False  # TODO detect
+if itIsWin:
+    from winsound import *
 
 CELL_SIZE = 16
 
@@ -9,6 +15,24 @@ HEIGHT = 20
 
 userX = 5
 userY = 1
+
+currentTick = 0
+
+
+def init_terrain():
+    global terrain
+    terrain = [[Terrain() for i in range(HEIGHT)] for i in range(WIDTH)]
+    for x in range(WIDTH):
+        for y in range(userY + 3):
+            terrain[x][y] = EmptySpace()
+        for y in range(userY + 3, userY + 4):
+            terrain[x][y] = Humus()
+        for y in range(userY + 4, userY + 7):
+            terrain[x][y] = Sand()
+        for y in range(userY + 7, HEIGHT):
+            terrain[x][y] = Diorite()
+
+init_terrain()
 
 window = Tk()
 window.wm_title("Копатель офлаен")
@@ -36,10 +60,18 @@ canvas.bind("<Button-1>", on_click)
 
 
 def draw():
+    def draw_rect_at(x, y, color, width=0):
+        x *= CELL_SIZE
+        y *= CELL_SIZE
+        canvas.create_rectangle(x, y, x + CELL_SIZE, y + CELL_SIZE, fill=color, width=width)
+
     canvas.delete("all")
-    x = userX * CELL_SIZE
-    y = userY * CELL_SIZE
-    canvas.create_rectangle(x, y, x + CELL_SIZE, y + CELL_SIZE, fill="#689F38", width=1)
+
+    for x in range(0, WIDTH):
+        for y in range(0, HEIGHT):
+            draw_rect_at(x, y, terrain[x][y].color())
+
+    draw_rect_at(userX, userY, PLAYER_COLOR, 1)
 
 
 def last_key_is(dir):
@@ -47,7 +79,7 @@ def last_key_is(dir):
 
 
 def update():
-    global userX, userY, last_key
+    global userX, userY, last_key, currentTick
     x = userX
     y = userY
     if last_key_is('s'):
@@ -60,11 +92,12 @@ def update():
         y -= 1
 
     # if x >= 0 and x < WIDTH and y >= 0 and y < HEIGHT:
-    if 0 <= x < WIDTH and 0 <= y < HEIGHT:
+    if 0 <= x < WIDTH and 0 <= y < HEIGHT and terrain[x][y].isAccessible():
         userX = x
         userY = y
 
     last_key = None
+    currentTick += 1
 
 
 def do_loop():
@@ -74,7 +107,10 @@ def do_loop():
 
 
 def play_sound_track():
+    if not itIsWin:
+        return
     PlaySound('cr1est.wav', SND_FILENAME)
+    pass
 
 
 thread = threading.Thread(target=play_sound_track)
